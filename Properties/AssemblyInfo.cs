bf21b354842b3ae54f30706e28d06afc85afff6c
David Andrew <damussel06@hotmail.com>
2016-08-07 07:21:01 -0500

    initTemplate
    Created by Bob Tabor @Dev University

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CS_ASP_017")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9b5988da-29ce-482f-a046-cbd271c22f56")]
+[assembly: Guid("e458f834-6843-4367-9e9e-5084dbaf8d24")]
 
 // Version information for an assembly consists of the following four values:
 //
